b51f8431fd8756413b8395c3b2ca2eb2a5a94337
acidtib <acidtib@protonmail.com>
2021-02-25 12:05:14 -0700

    unix sync from manifest

--- sugar_sync-unix.sh
+++ sugar_sync-unix.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
-# usage: ./sugar_sync-unix.sh ~/.sugarchain
+# usage: ./sugar_sync-unix.sh chain_path network
+# exmaple: ./sugar_sync-unix.sh ~/.sugarchain mainnet
 
 args=("$@")
 
-wget -x -nH -i manifest.txt --no-check-certificate --cut-dirs=1 --directory-prefix=${args[0]}
+chain="${args[0]}"
+network="${args[1]}"
+
+if [ -z "$chain" ]
+then
+  chain="~/.sugarchain"
+fi
+
+if [ -z "$network" ]
+then
+  network="mainnet"
+fi
+
+echo "[INFO:] starting ${network} sync into ${chain}"
+echo "..."
+echo ".."
+echo "."
+
+# download network chain
+wget -x -nH -i https://raw.githubusercontent.com/sugardigger/sugarchain_snapshot/main/manifest/${args[1]}.txt --no-check-certificate --cut-dirs=1 --directory-prefix=${args[0]}
+
+# remove temp manifest.txt
+rm -rf ${args[0]}/sugarchain_snapshot
